e0bdfef79d4a333b46866a7a4e96b79734ed51ac
Rbabar123 <108402307+Rbabar123@users.noreply.github.com>
2024-12-27 19:00:08 +0800

    Update songs.js

--- js/songs.js
+++ js/songs.js
@@ -1,9 +1,25 @@
+const songs = {
+	yungkai: 'audio/yungkai.mp3',
+	lilboo: 'audio/lilBoo.mp3',
+	beauty: 'audio/beauty.mp3'
+};
 
-			document.querySelector('.song-list').addEventListener('change', function(event) {
-				const audio = new Audio();
-				if (event.target.value === 'yungkai') {
-					audio.src = 'audio/yungkai.mp3';
-					audio.play();
-				}
-			});
-		
+let currentSongIndex = 0;
+const songKeys = Object.keys(songs);
+const audio = new Audio(songs.yungkai); // Set default song to "Blue by Yung Kai"
+audio.play(); // Autoplay the default song
+
+document.querySelector('.song-list').addEventListener('change', function(event) {
+	const selectedSong = event.target.value;
+	if (songs[selectedSong]) {
+		audio.src = songs[selectedSong];
+		audio.play();
+		currentSongIndex = songKeys.indexOf(selectedSong);
+	}
+});
+
+audio.addEventListener('ended', function() {
+	currentSongIndex = (currentSongIndex + 1) % songKeys.length;
+	audio.src = songs[songKeys[currentSongIndex]];
+	audio.play();
+});
